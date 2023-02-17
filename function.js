c3a03faf9539560289050060a87109d0603fa000
Gustavo A. Valero P <125612395+Gavp20@users.noreply.github.com>
2023-02-17 13:58:01 -0400

    Create function.js

---
+++ function.js
@@ -0,0 +1,129 @@
+// https://www.amcharts.com/demos/line-with-changing-color
+
+window.function = function (data, width, height) {
+
+  // data
+  data = data.value ?? "";
+  width = width.value ?? 100;
+  height = height.value ?? 500;
+ 
+  let ht = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Glide Yes-Code</title>
+	
+     <!-- Resources (HTML) -->
+     <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+     <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+     <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+  </head>
+  <body>
+  <div id="chartdiv"></div>
+  <!-- Styles (CSS) -->
+  <style>
+    #chartdiv {
+      width: ${width}%;
+      height: ${height}px;
+    }
+  </style>
+  
+<script>
+<!-- Chart code (JavaScript)  -->
+// Create root element
+var root = am5.Root.new("chartdiv");
+// Set themes
+root.setThemes([
+  am5themes_Animated.new(root)
+]);
+// Create chart
+var chart = root.container.children.push(am5xy.XYChart.new(root, {
+  panX: true,
+  panY: true,
+  wheelX: "panX",
+  wheelY: "zoomX",
+  layout: root.verticalLayout,
+  pinchZoomX:true
+}));
+// Add cursor
+var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+  behavior: "none"
+}));
+cursor.lineY.set("visible", false);
+var colorSet = am5.ColorSet.new(root, {});
+//   *** The data ***
+var data = [ ${data} ];
+// Create axes
+var xRenderer = am5xy.AxisRendererX.new(root, {});
+xRenderer.grid.template.set("visible", false);
+xRenderer.labels.template.setAll({
+  location: 0.5,
+  rotation: -90,
+  fontSize: 10,
+  multiLocation: 0.5
+});
+var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
+  categoryField: "time",  //***
+  renderer: xRenderer,
+  paddingRight:15, 
+  tooltip: am5.Tooltip.new(root, {})
+}));
+xAxis.data.setAll(data);
+var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+  maxPrecision: 0,
+  renderer: am5xy.AxisRendererY.new(root, {})
+}));
+yAxis.children.unshift(
+  am5.Label.new(root, {
+    rotation: -90,
+    text: "Average  SPX  Points",
+    y: am5.p50,
+    centerX: am5.p50
+  })
+);
+var series = chart.series.push(am5xy.LineSeries.new(root, {
+  xAxis: xAxis,
+  yAxis: yAxis,
+  valueYField: "value",
+  categoryXField: "time",     //***
+  tooltip: am5.Tooltip.new(root, {
+    labelText: "{valueY}",
+    dy:-5
+  })
+}));
+series.strokes.template.setAll({
+  templateField: "strokeSettings",
+  strokeWidth: 2
+});
+series.fills.template.setAll({
+  visible: true,
+  fillOpacity: 0.5,
+  templateField: "fillSettings"
+});
+series.bullets.push(function() {
+  return am5.Bullet.new(root, {
+    sprite: am5.Circle.new(root, {
+      templateField: "bulletSettings",
+      radius: 5
+    })
+  });
+});
+series.data.setAll(data);
+series.appear(1000);
+// Add scrollbar
+// https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+chart.set("scrollbarX", am5.Scrollbar.new(root, {
+  orientation: "horizontal",
+  marginBottom: 20
+}));
+// Make stuff animate on load
+// https://www.amcharts.com/docs/v5/concepts/animations/
+chart.appear(1000, 100);
+</script>
+  </body>
+</html>`
+
+  let enc = encodeURIComponent(ht);
+  let uri = `data:text/html;charset=utf-8,${enc}`
+  return uri; 
+}   
