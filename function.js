e6cb99eedf00610b43d6cb31548e49e90134091b
Gustavo A. Valero P <125612395+Gavp20@users.noreply.github.com>
2023-04-03 14:37:55 -0400

    Update function.js

--- function.js
+++ function.js
@@ -26,9 +26,9 @@ let ht = `<!DOCTYPE html>
 
         var data = google.visualization.arrayToDataTable([
           ['Label', 'Value'],
-          ['${labelVal1[0]}', ${labelVal1[2]}/2],
-          ['${labelVal2[0]}', ${labelVal2[2]}/2],
-          ['${labelVal3[0]}', ${labelVal3[2]}/2]
+          ['${labelVal1[0]}', ${labelVal1[2]}*0.7],
+          ['${labelVal2[0]}', ${labelVal2[2]}*0.6],
+          ['${labelVal3[0]}', ${labelVal3[2]}*0.75]
         ]);
 
         var options = {
